06b3342d507c267b5ebd6bbbb7d380ebcf82b80c
Christopher Hiew <christopherhiew@protonmail.com>
2023-05-04 15:20:18 +0100

    Removed redundant action.

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,8 +1,6 @@
 import { createAction } from "../../utilities/reducer/reducer.utils";
 import { CATEGORIES_ACTION_TYPES } from "./category.types";
 
-export const setCategories = (categories) => createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categories);
-
 export const fetchCategoriesStart = () => createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
 
 export const fetchCategoriesSuccess = (categories) => createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categories);
