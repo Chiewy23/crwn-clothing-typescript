29aea25b692c29c5858b2e008251aa51c8410725
Christopher Hiew <christopherhiew@protonmail.com>
2023-05-10 15:25:00 +0100

    Typed store.

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,17 +1,20 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 import { rootReducer } from './root-reducer';
-import { persistStore, persistReducer } from 'redux-persist';
+import { persistStore, persistReducer, PersistConfig } from 'redux-persist';
 import { rootSaga } from './root-saga';
 
 import storage from 'redux-persist/lib/storage';
 import createSagaMiddleware from "redux-saga";
 
 export type RootState = ReturnType<typeof rootReducer>;
+export type ExtendedPersistConfig = PersistConfig<RootState> & {
+    whitelist: (keyof RootState)[]
+};
 
-const persistConfig = {
+const persistConfig: ExtendedPersistConfig = {
     key: "root",        // persist everything
     storage: storage,   // use browser localStorage
-    blacklist: ["categories, user"]
+    whitelist: ["cart"]
 };
 
 const sagaMiddleware = createSagaMiddleware();
