ab143ab58e6897f632d6101d4ca4f026cded0230
Christopher Hiew <christopherhiew@protonmail.com>
2023-06-20 15:40:26 +0100

    Initial start to Category unit test.

---
+++ src/routes/category/__tests__/category.component.test.js
@@ -0,0 +1,10 @@
+import { screen } from "@testing-library/react";
+import { renderWithProviders } from "../../../utilities/test/test.utils";
+
+import Category from "../category.component";
+
+describe("category tests", () => {
+    test("it should render a Spinner if isLoading is true", () => {
+
+    });
+});
