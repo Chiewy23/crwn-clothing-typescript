074ffdc535542f706271b341773d1ce03b0044a4
Christopher Hiew <christopherhiew@protonmail.com>
2023-06-20 15:43:58 +0100

    Rendering Category component.

--- src/routes/category/__tests__/category.component.test.js
+++ src/routes/category/__tests__/category.component.test.js
@@ -3,8 +3,22 @@ import { renderWithProviders } from "../../../utilities/test/test.utils";
 
 import Category from "../category.component";
 
+jest.mock("react-router-dom", () => ({
+    ...jest.requireActual("react-router-dom"),
+    useParams: () => ({
+        category: "mens"
+    })
+}));
+
 describe("category tests", () => {
     test("it should render a Spinner if isLoading is true", () => {
-
+        renderWithProviders(<Category />, {
+            preloadedState: {
+                categories: {
+                    isLoading: true,
+                    categories: []
+                }
+            }
+        })
     });
 });
